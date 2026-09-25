49570ab48ed7b5853bcc1d73f0e5250836d96c98
Maryam Tarek <Maryamtarek784@gmail.com>
2026-09-25 23:03:51 +0200

    Enable mock API in production

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,15 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./i18n";
 import "./index.css";
-import App from "./App.jsx";
 import Providers from "./app/providers.jsx";
 import { router } from "./app/router.jsx";
 
 async function enableMocking() {
-  if (import.meta.env.DEV) {
-    const { worker } = await import("./mocks/browser");
+  const { worker } = await import("./mocks/browser");
 
-    await worker.start({
-      onUnhandledRequest: "bypass",
-    });
-  }
+  await worker.start({
+    onUnhandledRequest: "bypass",
+  });
 }
 
 enableMocking().then(() => {
