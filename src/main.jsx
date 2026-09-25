a0898c210ca1510e47245b97dd3f41fc9281dff3
Maryam Tarek <Maryamtarek784@gmail.com>
2026-09-25 23:20:12 +0200

    main

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,9 @@ async function enableMocking() {
   const { worker } = await import("./mocks/browser");
 
   await worker.start({
+    serviceWorker: {
+      url: "/mockServiceWorker.js",
+    },
     onUnhandledRequest: "warn",
   });
 }
